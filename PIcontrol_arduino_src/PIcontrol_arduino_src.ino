05f7bad9815798ba713d90b362e5b47468edff98
k16025kk <ono@pluslab.org>
2023-12-10 19:54:17 +0900

    タイマ割り込み実装

---
+++ PIcontrol_arduino_src/PIcontrol_arduino_src.ino
@@ -0,0 +1,31 @@
+/*
+ * ボード設定：Arduino DUE（Programming Port）
+ */
+
+void setup() {
+  
+  startTimer(TC1, 0, TC3_IRQn, 20);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// タイマ割り込み設定
+void startTimer(Tc *tc, uint32_t channel, IRQn_Type irq, uint32_t mSec) {
+  pmc_enable_periph_clk((uint32_t)irq);
+  TC_Configure(tc, channel, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK1);
+  uint32_t rc = (VARIANT_MCK/2/1000)*mSec;
+  TC_SetRC(tc, channel, rc);
+  TC_Start(tc, channel);
+  tc->TC_CHANNEL[channel].TC_IER=TC_IER_CPCS;
+  tc->TC_CHANNEL[channel].TC_IDR=~TC_IER_CPCS;
+  NVIC_EnableIRQ(irq);
+}
+
+
+void TC3_Handler() {
+  TC_GetStatus(TC1, 0);
+  // 割り込み発生時に実行する部分
+}
